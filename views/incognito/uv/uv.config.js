b1e8ffc21efe63874833bc7fad4a531a61f383d1
brettrm001 <brettrm001@gmail.com>
2022-06-27 22:07:23 -0400

    Bare Server URL Update

--- views/incognito/uv/uv.config.js
+++ views/incognito/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://bare.brettrm001.repl.co/',
+    bare: 'https://bare-server-1337.herokuapp.com/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
